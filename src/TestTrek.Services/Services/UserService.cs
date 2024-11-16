7729161a486539d5d8b3df8f2c7bc8688b0b92cf
Hritam Mondal <hritam.mondal@yahoo.com>
2024-11-16 20:54:09 +0530

    Add Serilog integration for logging

--- src/TestTrek.Services/Services/UserService.cs
+++ src/TestTrek.Services/Services/UserService.cs
@@ -1,14 +1,21 @@
 using Mapster;
+using Microsoft.Extensions.Logging;
 using TestTrek.Core.Dtos;
 using TestTrek.Core.Interfaces;
 
 namespace TestTrek.Services.Services;
 
-public class UserService(IUserRepository userRepository) : IUserService
+public class UserService(IUserRepository userRepository, ILogger<UserService> logger) : IUserService
 {
     public async Task<List<UserDto?>> GetAllUsersAsync()
     {
+        logger.LogInformation("Fetching all users from the database.");
         var users = await userRepository.GetAllUsersAsync();
-        return users.Adapt<List<UserDto?>>();
+        logger.LogDebug("Fetched {Count} users from the database.", users.Count);
+        
+        var userDtos = users.Adapt<List<UserDto?>>();
+        logger.LogInformation("Successfully mapped users to UserDto.");
+            
+        return userDtos;
     }
 }
